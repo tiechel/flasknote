d2a8118bb4534f0a00eb88b83ca8030be9c3d20c
tiechel <alicetype.luna@gmail.com>
2019-10-23 14:40:44 +0900

    Add Flask-Admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from config import Config
 from flask_migrate import Migrate
-from app.models import db
+from app.models import db, Entry
 
 migrate = Migrate()
+admin = Admin(name='flasknote', template_mode='bootstrap3')
+admin.add_view(ModelView(Entry, db.session))
 
 
 def create_app(config_class=Config):
@@ -11,6 +15,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
+    admin.init_app(app)
 
     from app.main.routes import bp as main_bp
     app.register_blueprint(main_bp)
