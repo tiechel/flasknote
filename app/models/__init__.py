de474acd10e85b870356225296866d492bece1ed
tiechel <alicetype.luna@gmail.com>
2019-10-25 10:43:35 +0900

    Add Fortune model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -11,3 +11,20 @@ class Entry(db.Model):
 
     def __repr___(self):
         return '<Entry {}>'.format(id)
+
+
+class Fortune(db.Model):
+    __tablename__ = 'fortunes'
+    __table_args__ = (db.UniqueConstraint('date', 'sign', name='unique_idx_date_sign'), {})
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    sign = db.Column(db.String(), nullable=False)
+    rank = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(), nullable=False)
+    item = db.Column(db.String(), nullable=False)
+    money = db.Column(db.Integer, nullable=False)
+    job = db.Column(db.Integer, nullable=False)
+    love = db.Column(db.Integer, nullable=False)
+    total = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(), nullable=False)
